77891f172764a1d1e8966abdc1a20fb0dc29f253
Gustavo <gustavodeveque123@gmail.com>
2022-06-05 17:06:44 -0300

    Configurando o Repositório

---
+++ Models/DtoPatrimonio.cs
@@ -0,0 +1,19 @@
+﻿namespace SysPatrimonio.Models
+{
+    public class DtoPatrimonio
+    {
+        public int id { get; set; }
+        public string numeroetiqueta { get; set; }
+        public string nomepatrimonio { get; set; }
+        public string descricaopatrimonio { get; set; }
+        public string valorpatrimonio { get; set; }
+        public string marcamodelo { get; set; }
+        public DateTime dataaquisicao { get; set; }
+        public DateTime databaixa { get; set; }
+        public int numnf { get; set; }
+        public string numserie { get; set; }
+        public int situacao { get; set; }
+
+
+    }
+}
